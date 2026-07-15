5fd6abb6098b22bcb3a7b2e042e54f6dc6f23769
Nishat-Annan-Mim <nishat.annan.mim.2003@gmail.com>
2026-07-15 15:10:08 +0600

    final layout

---
+++ src/app/faq/page.tsx
@@ -0,0 +1,9 @@
+import FAQSection from "@/components/FAQSection";
+
+export default function FAQPage() {
+  return (
+    <main className="flex-1 py-8 w-full">
+      <FAQSection />
+    </main>
+  );
+}
